0ac33cd1a18633e7527ee6662fbee6d18cdc3f35
DanielFrikel <daniel.aguilar193@tectijuana.edu.mx>
2023-02-20 23:42:20 -0800

    Promise finalizado

--- 33-Promises/js/03-app.js
+++ 33-Promises/js/03-app.js
@@ -1,4 +1,30 @@
-//Promises
-const aplicarDescuento = new Promise((resolve,)=>{
-    
-});
+//Promises, resolve se ejecuta cuando se ejecuta bien el promise
+//o cuando se cumple y reject cuando hay un error.
+const aplicarDescuento = new Promise((resolve,reject)=>{
+    const descuento = true;
+    if(descuento){
+        resolve('Descuento aplicado');
+    }else{
+        reject('No se pudo aplicar el descuento');
+    }
+})
+
+aplicarDescuento
+    .then(resultado =>descuento(resultado))
+    //En caso de que el promise se ejecute correctamente se hara
+    //lo siguiente:
+    //.then(arrowfunction)
+    .catch(error => console.log(error))
+    //En caso de que el promise de error se hara lo siguiente
+    //.catch(arrowfunction)
+
+// console.log(aplicarDescuento);
+
+//Hay 3 valores posibles
+//fullfilled = El promise se cumplio correctamente
+//rejected = El promise NO se cumplio.
+//pending = El promise aun no se cumple ni se rechazo.
+
+function descuento(mensaje){
+    console.log(mensaje);
+}
